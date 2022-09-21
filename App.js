2094ac9595325a7df01ab37d79978e53627e9cf8
Pallavi1603 <78224696+Pallavi1603@users.noreply.github.com>
2022-09-21 11:14:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+import Calculator from './Calculator';
+
+
+const App=()=>{
+    return (
+       <View>        
+        <Calculator />
+       </View>
+
+    )
+}
+
+export default App;
